9087d84d3400781725fc44fc5706b7f3ed3a0354
code-architect <codearchitectdeveloper@gmail.com>
2020-04-22 23:38:23 +0530

    created internal function and a static function to generate verification code in User Model, made changes in Transaction model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,21 +9,34 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    const VERIFIED_USER = '1';
+    const UNVERIFIED_USER = '0';
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'verified', 'verification_token', 'admin'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'verification_token'
     ];
+
+
+    //----------------------------------- Internal Helper Methods ------------------------------------------------//
+    public function isVerified()
+    {
+        return $this->verified = User::VERIFIED_USER;
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin = User::ADMIN_USER;
+    }
+
+    public static function  generateVerificationCode()
+    {
+        return str_random(40);
+    }
+    //------------------------------------------------------------------------------------------------------------//
 }
