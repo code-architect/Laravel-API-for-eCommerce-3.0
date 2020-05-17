dd2a9456035c0897e8cdd0868aee113f4b82e32a
code-architect <codearchitectdeveloper@gmail.com>
2020-05-17 18:36:27 +0530

    added exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,21 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -44,7 +53,53 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($exception instanceof ValidationException)
+        {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        if($exception instanceof ModelNotFoundException)
+        {
+            // getting the class name where error has occurred
+            $modelName = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Does not exists any {$modelName} with the specified identifications", 404);
+        }
+        if($exception instanceof AuthenticationException)
+        {
+            // Listen to the AuthenticationException directly
+            return $this->unauthenticated($request, $exception);
+        }
+        if($exception instanceof AuthorizationException)
+        {
+            // the user/client do not have enough permission
+            return $this->errorResponse($exception->getMessage(), 403);
+        }
+        if($exception instanceof NotFoundHttpException)
+        {
+            return $this->errorResponse("This specified URL cannot be found", 404);
+        }
+        if($exception instanceof MethodNotAllowedHttpException)
+        {
+            return $this->errorResponse("This specified method for the request is invalid", 405);
+        }
+        if($exception instanceof HttpException)
+        {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+        if($exception instanceof QueryException)
+        {
+            // fetching the specific error code
+            $errorCode = $exception->errorInfo[1];
+            if($errorCode == 1451)
+            {
+                return $this->errorResponse('Cannot remove this resource permanently. It is related with any other resource', 409);
+            }
+        }
+        // check if in debug , then return the detail error
+        if(config('app.debug')){
+            return parent::render($request, $exception);
+        }
+        return $this->errorResponse("Unexpected Exception. try later", 500);
+
     }
 
     /**
@@ -56,10 +111,20 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
+        return $this->errorResponse('Unauthenticated.', 401);
+    }
 
-        return redirect()->guest(route('login'));
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors, 422);
     }
 }
