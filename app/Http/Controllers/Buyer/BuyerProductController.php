cf3d8a74c5a4e95028700b7fa3ba0bd327a8ccba
code-architect <codearchitectdeveloper@gmail.com>
2020-05-30 15:10:19 +0530

    created BuyerProductController and added respective routes, obtaining list of products by a specific buyers using Eager loading, using pluck() method to only get the list of products(to get all the transaction their list of products remove pluck('prodduct')

---
+++ app/Http/Controllers/Buyer/BuyerProductController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers\Buyer;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BuyerProductController extends ApiController
+{
+
+    public function index(Buyer $buyer)
+    {
+        // The result of the transaction relationship is a collection, because a buyer have many transactions
+        // so for this we are using Eager loading, to obtain the product directly within every transaction
+        //For this we need to obtain the query builder from the relationship and not the relationship itself.
+        // By doing this we are obtaining the list of transactions and with every transaction is going to come
+        // with their respective products.
+        $products = $buyer->transactions()->with('product')->get()->pluck('product');
+        return $this->showAll($products);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Http\Request;
 /** Buyers */
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
 /** Sellers */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 /** Categories */
