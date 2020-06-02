abd93a5330852ac05515f1b59c0ece44f539f391
code-architect <codearchitectdeveloper@gmail.com>
2020-06-02 14:37:42 +0530

    created SellerBuyerController and routes, Getting all the buyers of a single seller

---
+++ app/Http/Controllers/Seller/SellerBuyerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SellerBuyerController extends ApiController
+{
+    /**
+     * Getting all the buyers of a single seller
+     * @param Seller $seller
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Seller $seller)
+    {
+        // this relation is same as category buyer
+        $buyers = $seller->products()->whereHas('transactions')
+                    ->with('transactions.buyer')->get()
+                    ->pluck('transactions')->collapse()      // we are going to obtain several collections into one, so we need to make a single collection
+                    ->pluck('buyer')->unique('id')
+                    ->values();
+        return $this->showAll($buyers);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' =>
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
 
 /** Categories */
 Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
