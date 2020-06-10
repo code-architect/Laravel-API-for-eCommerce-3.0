5db32657f55307d9476a999bf4a96332df77fc57
code-architect <codearchitectdeveloper@gmail.com>
2020-06-10 06:26:34 +0530

    making the mailables markdown friendly"

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -1,3 +1,12 @@
-Hello {{$user->name}},
-You have changed your email id. Please verify your new email using this link:
-{{route('verify', $user->verification_token)}}
+@component('mail::message')
+    # Hello {{$user->name}}
+
+    You have changed your email id. Please verify your new email using button link:
+
+    @component('mail::button', ['url' => route('verify', $user->verification_token)])
+        Verify Account
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
