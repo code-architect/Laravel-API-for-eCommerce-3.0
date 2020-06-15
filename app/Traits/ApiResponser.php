1ae88968b91bc2dcd51a998d3704fbefd842ee50
code-architect <codearchitectdeveloper@gmail.com>
2020-06-16 04:43:17 +0530

    Sorting results by any given attribute

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -7,16 +7,36 @@ use Illuminate\Support\Collection;
 
 trait  ApiResponser
 {
+    /**
+     * Return a success response in JSOn format
+     * @param $data
+     * @param $code
+     * @return \Illuminate\Http\JsonResponse
+     */
     private function successResponse($data, $code)
     {
         return response()->json($data, $code);
     }
 
+
+    /**
+     * Return error response in JSON format
+     * @param $message
+     * @param $code
+     * @return \Illuminate\Http\JsonResponse
+     */
     protected function errorResponse($message, $code)
     {
         return response()->json(['error' => $message, 'code' => $code], $code);
     }
 
+
+    /**
+     * Return multiple records
+     * @param Collection $collection
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
     protected function showAll(Collection $collection, $code = 200)
     {
         if ($collection->isEmpty()) {
@@ -24,10 +44,18 @@ trait  ApiResponser
         }
 
         $transformer = $collection->first()->transformer;
+        $collection  =$this->sortData($collection);
         $collection = $this->transformData($collection, $transformer);
         return $this->successResponse($collection, $code);
     }
 
+
+    /**
+     * Return a single record
+     * @param Model $model
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
     protected function showOne(Model $model, $code = 200)
     {
         $transformer = $model->transformer;
@@ -35,11 +63,35 @@ trait  ApiResponser
         return $this->successResponse($model, $code);
     }
 
+
+    /**
+     * Show a message
+     * @param $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
     }
 
+
+    protected function sortData(Collection $collection)
+    {
+        if(request()->has('sort_by'))
+        {
+            $attribute = request()->sort_by;
+            $collection = $collection->sortBy->{$attribute};
+        }
+        return $collection;
+    }
+
+    /**
+     * Transform the data using the respected Transformers and change the return type of the queried data
+     * @param $data
+     * @param $transformer
+     * @return array
+     */
     protected function transformData($data, $transformer)
     {
         $transformation = fractal($data, new $transformer);
