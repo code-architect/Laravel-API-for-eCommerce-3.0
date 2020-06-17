d546342a8427e17065ae6c21240a13d28a8d517c
code-architect <codearchitectdeveloper@gmail.com>
2020-06-18 04:24:32 +0530

    removed register controller, link because we are going to use register from the API

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,18 @@ $this->post('login', 'Auth\LoginController@login');
 $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
-$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-$this->post('register', 'Auth\RegisterController@register');
 
 // Password Reset Routes...
 $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function (){
+    return view('welcome');
+})->middleware('guest')->name('welcome');
+
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
