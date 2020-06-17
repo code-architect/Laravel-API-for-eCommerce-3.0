ff8cf8d47af3340bd071842f8bb04143848615ed
code-architect <codearchitectdeveloper@gmail.com>
2020-06-18 04:14:53 +0530

    run php artisan make:auth to create a basic level of UI, replaced Auth::routes() in routes/web.php with original list of routes from vendor/laravel/framework/src/Illuminate/Routing/Router.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
+
+//vendor/laravel/framework/src/Illuminate/Routing/Router.php
+
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get('/home', 'HomeController@index')->name('home');
