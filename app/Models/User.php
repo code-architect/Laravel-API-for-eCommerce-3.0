a60cc9f75df425d73df0435a9440ca6db1f03a10
code-architect <codearchitectdeveloper@gmail.com>
2020-06-20 16:59:29 +0530

    7. Registering the required routes for passport, making User to use passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use App\Transformers\UserTransformer;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, HasApiTokens, SoftDeletes;
     protected $dates = ['deleted_at'];
 
     public $transformer = UserTransformer::class;
--- routes/api.php
+++ routes/api.php
@@ -50,3 +50,5 @@ Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]
 Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
 Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
 
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
+
